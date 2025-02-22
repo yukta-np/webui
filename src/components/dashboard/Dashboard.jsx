ab52d8d727008fa8132eebd052f3c1b85363c044
Sirshkoirala <seris.koerala@gmail.com>
2025-02-23 00:15:48 +0545

    Add initial project setup with Prettier configuration, new components, and updated styles

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+const items = [
+  getItem('Dashboard', '1', <PieChartOutlined />),
+  getItem('Routine', '2', <DesktopOutlined />),
+  getItem('Tasks', '3', <UserOutlined />, [
+    getItem('My Tasks', '4'),
+    getItem("My Team's Tasks", '5'),
+    getItem('All Tasks', '6'),
+  ]),
+  getItem('Leave Request', '7', <TeamOutlined />, [
+    getItem('My Leave Request', '8'),
+    getItem("My Team's Leave Request", '9'),
+  ]),
+  getItem('Documents', '10', <FileOutlined />),
+  getItem('Announcements', '11', <FileOutlined />),
+  getItem('Calender', '12', <FileOutlined />),
+  getItem('Settings', '13', <FileOutlined />),
+];
+const Dashboard = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider
+        breakpoint="lg"
+        collapsedWidth={65}
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={['1']}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default Dashboard;
