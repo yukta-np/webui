1ba7aff834ebefa0ea2ecccced2e0d57b55af922
Dip Ojha <dipojha03@gmail.com>
2025-02-28 17:01:09 +0545

    Update Next.js to version 15.2.0 and enhance AddTask component with form validation

--- src/components/popup/AddTask.jsx
+++ src/components/popup/AddTask.jsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
-import { Button, Flex, Modal } from 'antd';
+import { Button, Flex, Modal, Space, Form, Input, Radio } from 'antd';
+import { InfoCircleOutlined } from '@ant-design/icons';
 
+const customizeRequiredMark = (label, { required }) => (
+  <>
+    {required ? (
+      <Tag color="error">Required</Tag>
+    ) : (
+      <Tag color="warning">optional</Tag>
+    )}
+    {label}
+  </>
+);
 const AddTask = () => {
     const [open, setOpen] = useState(false);
     const [openResponsive, setOpenResponsive] = useState(false);
-  return (
-   <Flex vertical gap="middle" align="flex-start">
-   
+
+    const [form] = Form.useForm();
+  const [requiredMark, setRequiredMarkType] = useState('optional');
+  const onRequiredTypeChange = ({ requiredMarkValue }) => {
+    setRequiredMarkType(requiredMarkValue);
+  };
+    return (
+    <Flex vertical gap="middle" align="flex-start">
       {/* Responsive */}
       <Button type="primary" onClick={() => setOpenResponsive(true)}>
         Add Task
       </Button>
+      {/* Whole form goes here */}
       <Modal
-        title="Modal responsive width"
+        title="Task Form"
         centered
         open={openResponsive}
         onOk={() => setOpenResponsive(false)}
@@ -26,9 +43,40 @@ const AddTask = () => {
           xxl: '40%',
         }}
       >
-        <p>some contents...</p>
-        <p>some contents...</p>
-        <p>some contents...</p>
+        {/* input form goes here */}
+        <Space>
+          {/* Left side */}
+          <Form
+            form={form}
+            layout="vertical"
+            initialValues={{
+              requiredMarkValue: requiredMark,
+            }}
+            onValuesChange={onRequiredTypeChange}
+            requiredMark={
+              requiredMark === 'customize'
+                ? customizeRequiredMark
+                : requiredMark
+            }
+          >
+            <Form.Item
+              label="Title"
+              name="title"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the title of the task!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            </Form>
+          <Space></Space>
+
+          {/* Right side */}
+          <Space></Space>
+        </Space>
       </Modal>
     </Flex>
   );
