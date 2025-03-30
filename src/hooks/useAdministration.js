49364a6a30c99385b495d10f3cd2fc92343b9f47
dipojha <dipojha@users.noreply.github.com>
2025-03-30 13:27:57 +0545

    feat: Add AdministrationForm component and related hooks for administration management

---
+++ src/hooks/useAdministration.js
@@ -0,0 +1,36 @@
+// This file is part of the webui project.
+import { constants } from '@/constants';
+import useSWR from 'swr';
+import { fetcher, disableRefetchBlock } from '../utils';
+import { useMemo } from 'react';
+
+export function useAdministration(params) {
+   const queryString = useMemo(() => {
+      if (!params || Object.keys(params).length === 0) return '';
+      return new URLSearchParams(params).toString();
+   }, [params]);
+
+   const URL = constants.urls.administrationUrl;
+   const fullUrl = queryString ? `${URL}?${queryString}` : URL;
+
+   const { disableAutoRefetch } = params || {};
+   const autoRefetchConfig = disableAutoRefetch ? disableRefetchBlock : null;
+
+   const {
+      data: responseData,
+      error,
+      isValidating,
+      mutate,
+   } = useSWR(fullUrl, fetcher, autoRefetchConfig);
+
+   const revalidate = () => mutate(fullUrl);
+
+   return {
+      administrationData: responseData,
+      meta: responseData?.meta,
+      isLoading: isValidating,
+      isError: error,
+      revalidate,
+   };
+}
+
--- src/services/administration.http.js
+++ src/services/administration.http.js
@@ -1,16 +1,37 @@
-import axios from 'axios';
+
+
+// app/http/administration.http.js
 import { constants, headers } from '@/constants';
+
+import axios from 'axios';
 
 const URL = constants.urls.administrationUrl;
 
-export async function createTask(task) {
-  return axios.post(URL, task, { headers });
+export async function createAdmin(admin) {
+  return axios.post(URL, admin, { headers });
 }
 
-export async function updateTask(id, task) {
-  return axios.patch(`${URL}/${id}`, task, { headers });
+export async function updateAdmin(id, admin) {
+  return axios.patch(`${URL}/${id}`, admin, { headers });
 }
 
-export async function deleteTask(id) {
+export async function deleteAdmin(id) {
   return axios.delete(`${URL}/${id}`, { headers });
 }
+
+export async function createAdminComment(id, comment) {
+  return axios.post(`${URL}/${id}/comment`, comment, { headers });
+}
+
+export async function getAdminComments(id) {
+  return axios.get(`${URL}/${id}/comment`, { headers });
+}
+
+export async function updateAdminComment(id) {
+  return axios.patch(`${URL}/${id}/comment`, { headers });
+}
+
+export async function deleteAdminComment(id, commentId) {
+  return axios.delete(`${URL}/${id}/comment/${commentId}`, { headers });
+}
+
