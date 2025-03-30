26e8f0e276891a5cb1cdf43623a350d6a0aafb6d
abishekghimire <abishekghimire220.com>
2025-03-30 14:49:28 +0545

    feat: Update ControlCenter component with OrganisationDetails and OrganisationPlan components

--- src/pages/organisations/[id]/index.jsx
+++ src/pages/organisations/[id]/index.jsx
@@ -1,8 +1,13 @@
 import ControlCenter from '@/components/organisations/ControlCenter';
+import OrganisationDetails from '@/components/organisations/OrganisationDetails';
 import React from 'react';
 
 const index = () => {
-  return <ControlCenter />;
+  return (
+    <ControlCenter>
+      <OrganisationDetails />
+    </ControlCenter>
+  );
 };
 
 export default index;
