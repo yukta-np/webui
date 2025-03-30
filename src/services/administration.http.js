98f106d6f2e847c4b9b2808053afc4ec93b84258
dipojha <dipojha@users.noreply.github.com>
2025-03-30 13:36:56 +0545

    feat: Update administration service functions and replace Settings component with AdministrationList

--- src/services/administration.http.js
+++ src/services/administration.http.js
@@ -7,31 +7,31 @@ import axios from 'axios';
 
 const URL = constants.urls.administrationUrl;
 
-export async function createAdmin(admin) {
-  return axios.post(URL, admin, { headers });
+export async function createAdministration(administration) {
+  return axios.post(URL, administration, { headers });
 }
 
-export async function updateAdmin(id, admin) {
-  return axios.patch(`${URL}/${id}`, admin, { headers });
+export async function updateAdministration(id, administration) {
+  return axios.patch(`${URL}/${id}`, administration, { headers });
 }
 
-export async function deleteAdmin(id) {
+export async function deleteAdministration(id) {
   return axios.delete(`${URL}/${id}`, { headers });
 }
 
-export async function createAdminComment(id, comment) {
+export async function createAdministrationComment(id, comment) {
   return axios.post(`${URL}/${id}/comment`, comment, { headers });
 }
 
-export async function getAdminComments(id) {
+export async function getAdministrationComments(id) {
   return axios.get(`${URL}/${id}/comment`, { headers });
 }
 
-export async function updateAdminComment(id) {
+export async function updateAdministrationComment(id) {
   return axios.patch(`${URL}/${id}/comment`, { headers });
 }
 
-export async function deleteAdminComment(id, commentId) {
+export async function deleteAdministrationComment(id, commentId) {
   return axios.delete(`${URL}/${id}/comment/${commentId}`, { headers });
 }
 
