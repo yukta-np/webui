5c969dc412bf3cd6929c66bd6f0c76e2dca55230
abishekghimire <abishekghimire220.com>
2025-04-04 00:16:35 +0545

    feat: Update page titles in staffs and teachers index pages

--- src/pages/staffs/index.jsx
+++ src/pages/staffs/index.jsx
@@ -1,21 +1,17 @@
 // pages/settings/[id]/index.jsx
 import { useRouter } from 'next/router';
 import CustomHead from '@/components/customHead/CustomHead';
-import Settings from '@/components/settings/Settings';
 
 export default function Page() {
   const router = useRouter();
   const { staffs } = router.query; // Get student from router
 
- 
   const currentType = staffs || '';
 
   return (
     <>
       <CustomHead
-        actualTitle={`Staff - ${
-          staffs ? `${staffs} Profile` : 'All Staffs'
-        }`}
+        actualTitle={`Staff - ${staffs ? `${staffs} Profile` : 'All Staffs'}`}
       />
       <Settings currentType="staffs" />
     </>
