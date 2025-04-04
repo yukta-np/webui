13f1b6811d3c9535cb58fbb8ad974e346bb2d514
abishekghimire <abishekghimire220.com>
2025-04-04 10:30:51 +0545

    chore: Update breadcrumb links in academic pages

--- src/pages/data-sources/index.jsx
+++ src/pages/data-sources/index.jsx
@@ -7,31 +7,31 @@ const { Content } = Layout;
 const { useBreakpoint } = Grid;
 
 const index = () => {
-     const screens = useBreakpoint();
-     const {
-       token: { colorBgContainer, borderRadiusLG },
-     } = theme.useToken();
+  const screens = useBreakpoint();
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   return (
     <>
-     <CustomHead actualTitle="Data Sources" />
-       <Content style={{ margin: screens.xs ? '0 8px' : '0 16px' }}>
-      <Breadcrumb style={{ margin: '16px ' }}>
-        <Breadcrumb.Item>
-          <Link href="/">Dashboard</Link>
-        </Breadcrumb.Item>
-        <Breadcrumb.Item>Data Sources</Breadcrumb.Item>
-      </Breadcrumb>
-      <div
-        style={{
-          padding: screens.xs ? 16 : 24,
-          minHeight: 360,
-          background: colorBgContainer,
-          borderRadius: borderRadiusLG,
-        }}
-      >
-        <Datasources />
-      </div>
-    </Content>
+      <CustomHead actualTitle="Data Sources" />
+      <Content style={{ margin: screens.xs ? '0 8px' : '0 16px' }}>
+        <Breadcrumb style={{ margin: '16px ' }}>
+          <Breadcrumb.Item>
+            <Link href="/">Home</Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>Data Sources</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          style={{
+            padding: screens.xs ? 16 : 24,
+            minHeight: 360,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <Datasources />
+        </div>
+      </Content>
     </>
   );
 };
