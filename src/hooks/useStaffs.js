5cc0a870bad23895203affaff0bcb89fa5466b7b
dipojha <dipojha@users.noreply.github.com>
2025-04-04 14:02:18 +0545

    feat: Add staff management hooks and HTTP service for CRUD operations

---
+++ src/hooks/useStaffs.js
@@ -0,0 +1,34 @@
+// This file is part of the webui project.
+import { constants } from '@/constants';
+import useSWR from 'swr';
+import { fetcher, disableRefetchBlock } from '../utils';
+import { useMemo } from 'react';
+
+export function useStaffs(params) {
+   const queryString = useMemo(() => {
+      if (!params || Object.keys(params).length === 0) return '';
+      return new URLSearchParams(params).toString();
+   }, [params]);
+   const URL = constants.urls.staffUrl;
+   const fullUrl = queryString ? `${URL}?${queryString}` : URL;
+
+   const { disableAutoRefetch } = params || {};
+   const autoRefetchConfig = disableAutoRefetch ? disableRefetchBlock : null;
+
+   const {
+      data: responseData,
+      error,
+      isValidating,
+      mutate,
+   } = useSWR(fullUrl, fetcher, autoRefetchConfig);
+
+   const revalidate = () => mutate(fullUrl);
+
+   return {
+      staffs: responseData,
+      meta: responseData?.meta,
+      isLoading: isValidating,
+      isError: error,
+      revalidate,
+   };
+}
