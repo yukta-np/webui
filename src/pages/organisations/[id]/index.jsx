ef67eb89734a7516809abef3525ba1de355184c6
abishekghimire <abishekghimire220.com>
2025-04-05 14:44:50 +0545

    feat: Add breadcrumb navigation to organisation detail pages

--- src/pages/organisations/[id]/index.jsx
+++ src/pages/organisations/[id]/index.jsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/router';
 import CustomHead from '@/components/customHead/CustomHead';
 import { Breadcrumb, Layout, Grid, theme } from 'antd';
 import Link from 'next/link';
+import { useAppContext } from '@/app-context';
+import { Roles } from '@/utils';
 
 const { useBreakpoint } = Grid;
 const { Content } = Layout;
@@ -16,13 +18,22 @@ const index = () => {
   } = theme.useToken();
   const router = useRouter();
   const id = router.query.id;
+  const { loggedInUser } = useAppContext();
+  console.log(loggedInUser?.role);
   return (
     <>
       <Breadcrumb style={{ margin: '16px ' }}>
         <Breadcrumb.Item>
           <Link href="/dashboard">Home</Link>
         </Breadcrumb.Item>
-        <Breadcrumb.Item>Organisation Detail</Breadcrumb.Item>
+        {loggedInUser?.role === Roles.SYSADMIN ? (
+          <Breadcrumb.Item>
+            <Link href="/organisations">Organisations</Link>
+          </Breadcrumb.Item>
+        ) : (
+          <Breadcrumb.Item>Organisation</Breadcrumb.Item>
+        )}
+        <Breadcrumb.Item>Details</Breadcrumb.Item>
       </Breadcrumb>
       <Content style={{ margin: screens.xs ? '0 8px' : '0 16px' }}>
         <div
--- src/pages/organisations/[id]/settings.jsx
+++ src/pages/organisations/[id]/settings.jsx
@@ -1,12 +1,44 @@
 import CustomHead from '@/components/customHead/CustomHead';
 import OrganisationLayout from '@/components/organisations/OrganisationLayout';
 import React from 'react';
+import { Breadcrumb, Layout, Grid, theme } from 'antd';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const { useBreakpoint } = Grid;
+const { Content } = Layout;
 
 const settings = () => {
+  const screens = useBreakpoint();
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  const router = useRouter();
+  const id = router.query.id;
   return (
     <>
-      <CustomHead actualTitle="Organisation Settings" />
-      <OrganisationLayout>Settings</OrganisationLayout>;
+      <Breadcrumb style={{ margin: '16px ' }}>
+        <Breadcrumb.Item>
+          <Link href="/dashboard">Home</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>
+          <Link href="/organisations">Organisations</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>Settings</Breadcrumb.Item>
+      </Breadcrumb>
+      <Content style={{ margin: screens.xs ? '0 8px' : '0 16px' }}>
+        <div
+          style={{
+            padding: screens.xs ? 16 : 24,
+            minHeight: 360,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <CustomHead actualTitle="Organisation Settings" />
+          <OrganisationLayout>Settings</OrganisationLayout>;
+        </div>
+      </Content>
     </>
   );
 };
